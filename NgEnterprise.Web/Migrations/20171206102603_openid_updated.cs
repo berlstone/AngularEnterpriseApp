42b1cc8f4d862fd6f89ca0fb4add84222aa2e8a0
Clinton Beyerle <berlstone@hotmail.com>
2017-12-07 15:09:18 +0400

    Add project files.

---
+++ NgEnterprise.Web/Migrations/20171206102603_openid_updated.cs
@@ -0,0 +1,272 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace NgEnterprise.Web.Migrations
+{
+    public partial class openid_updated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictApplications_ApplicationId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OpenIddictApplications_ClientId",
+                table: "OpenIddictApplications");
+
+            migrationBuilder.RenameColumn(
+                name: "Start",
+                table: "OpenIddictTokens",
+                newName: "ExpirationDate");
+
+            migrationBuilder.RenameColumn(
+                name: "End",
+                table: "OpenIddictTokens",
+                newName: "CreationDate");
+
+            migrationBuilder.RenameColumn(
+                name: "Scope",
+                table: "OpenIddictAuthorizations",
+                newName: "Scopes");
+
+            migrationBuilder.RenameColumn(
+                name: "RedirectUri",
+                table: "OpenIddictApplications",
+                newName: "RedirectUris");
+
+            migrationBuilder.RenameColumn(
+                name: "LogoutRedirectUri",
+                table: "OpenIddictApplications",
+                newName: "PostLogoutRedirectUris");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "OpenIddictTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "OpenIddictTokens",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "OpenIddictScopes",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "OpenIddictScopes",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "OpenIddictAuthorizations",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "OpenIddictAuthorizations",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "OpenIddictAuthorizations",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "OpenIddictAuthorizations",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictApplications",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientId",
+                table: "OpenIddictApplications",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "OpenIddictApplications",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpenIddictApplications_ClientId",
+                table: "OpenIddictApplications",
+                column: "ClientId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictApplications_ApplicationId",
+                table: "OpenIddictTokens",
+                column: "ApplicationId",
+                principalTable: "OpenIddictApplications",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId",
+                table: "OpenIddictTokens",
+                column: "AuthorizationId",
+                principalTable: "OpenIddictAuthorizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictApplications_ApplicationId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OpenIddictApplications_ClientId",
+                table: "OpenIddictApplications");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "OpenIddictScopes");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "OpenIddictScopes");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "OpenIddictAuthorizations");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "OpenIddictAuthorizations");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "OpenIddictApplications");
+
+            migrationBuilder.RenameColumn(
+                name: "ExpirationDate",
+                table: "OpenIddictTokens",
+                newName: "Start");
+
+            migrationBuilder.RenameColumn(
+                name: "CreationDate",
+                table: "OpenIddictTokens",
+                newName: "End");
+
+            migrationBuilder.RenameColumn(
+                name: "Scopes",
+                table: "OpenIddictAuthorizations",
+                newName: "Scope");
+
+            migrationBuilder.RenameColumn(
+                name: "RedirectUris",
+                table: "OpenIddictApplications",
+                newName: "RedirectUri");
+
+            migrationBuilder.RenameColumn(
+                name: "PostLogoutRedirectUris",
+                table: "OpenIddictApplications",
+                newName: "LogoutRedirectUri");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictTokens",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "OpenIddictTokens",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "OpenIddictAuthorizations",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "OpenIddictAuthorizations",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictApplications",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientId",
+                table: "OpenIddictApplications",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpenIddictApplications_ClientId",
+                table: "OpenIddictApplications",
+                column: "ClientId",
+                unique: true,
+                filter: "[ClientId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictApplications_ApplicationId",
+                table: "OpenIddictTokens",
+                column: "ApplicationId",
+                principalTable: "OpenIddictApplications",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId",
+                table: "OpenIddictTokens",
+                column: "AuthorizationId",
+                principalTable: "OpenIddictAuthorizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
